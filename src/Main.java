34967d829d3c014ff1e2824c0a2b8666e168a872
Clarence Cesante <clarenceccesante@gmail.com>
2025-08-05 19:46:30 -0400

    original ver with just userinput, on some ics3u level

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        boolean valid = false;
+        Scanner input = new Scanner(System.in);
+        int num;
+        double sum = 0;
+
+        while(true){
+            try{
+                System.out.print("Enter the number of courses you are taking: ");
+                num = input.nextInt();
+                break; // break2end
+            }
+
+            catch (InputMismatchException n){
+                System.out.println("Invalid input. Please enter a valid integer.");
+                input.next(); // clears any invalid input
+            }
+        }
+
+
+        while(true){
+            try{
+                for(int i = 0; i < num; i++){
+                    System.out.print("Enter your mark: ");
+                    double grade = input.nextDouble();
+
+                    sum += grade;
+
+                }
+
+                break;
+            }
+
+            catch (InputMismatchException n){
+                System.out.println("Invalid input. Please enter a valid double.");
+                input.next(); // clears any invalid input
+            }
+        }
+
+
+        System.out.printf("Your average of " + num + " subjects: %.2f", (sum / (double) num));
+    }
+}
