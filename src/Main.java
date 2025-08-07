805b6da88c4cbde831e99c1958153a1c9116919c
Clarence Cesante <clarenceccesante@gmail.com>
2025-08-06 20:04:53 -0400

    redundant scanner in main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        Scanner input = new Scanner(System.in);
         GradeChecker g = new GradeChecker();
 
         g.checkGrades();
